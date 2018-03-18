63c31f82f83dd482612e2e790d7dc8d214be7c96
Satoshi Nobutsugu <s-nobutsugu@Satoshi-no-MacBook-Air.local>
2018-03-18 20:48:49 +0900

    実習 fragment

---
+++ AndroidStudio/practice/fundamentals/2nd/FragmentPractice/Practice1/app/src/main/java/jp/mixi/practice/fragment/beg/FragmentMain.java
@@ -0,0 +1,26 @@
+package jp.mixi.practice.fragment.beg;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by s-nobutsugu on 2018/03/18.
+ */
+
+public class FragmentMain extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
