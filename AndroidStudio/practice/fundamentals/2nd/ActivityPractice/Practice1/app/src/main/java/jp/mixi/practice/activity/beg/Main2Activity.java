1d1f510926f4e43999690347de2320e966f68fc6
Satoshi Nobutsugu <s-nobutsugu@Satoshi-no-MacBook-Air.local>
2018-03-18 16:20:49 +0900

    実習 Activity 1-1, 1-2

---
+++ AndroidStudio/practice/fundamentals/2nd/ActivityPractice/Practice1/app/src/main/java/jp/mixi/practice/activity/beg/Main2Activity.java
@@ -0,0 +1,16 @@
+package jp.mixi.practice.activity.beg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by s-nobutsugu on 2018/03/18.
+ */
+
+public class Main2Activity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
